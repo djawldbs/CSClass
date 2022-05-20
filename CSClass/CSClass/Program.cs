c82bed921184f601b1cfc0fdfe73b084401ab192
지윤 엄 <skan1090@naver.com>
2022-05-20 10:03:26 +0900

    #1 XML주석 달기
    /// 사용

--- CSClass/CSClass/Program.cs
+++ CSClass/CSClass/Program.cs
@@ -22,6 +22,30 @@ namespace CSClass
             Console.WriteLine(random.NextDouble());
             Console.WriteLine(random.NextDouble()*10);
 
+            /*
+            List<int> list = new List<int>();
+            list.Add(52);
+            list.Add(273);
+            list.Add(32);
+            list.Add(64);
+            */
+
+             List<int> list = new List<int>() { 52,273,32,24};
+            list.Remove(52);
+
+
+            foreach(var item in list)
+            {
+                Console.WriteLine("Count: "+list.Count+"\titem: "+item);
+            }
+
+            Console.WriteLine(Math.Abs(-52273));
+            Console.WriteLine(Math.Ceiling(52.273));
+            Console.WriteLine(Math.Floor(52.273));
+            Console.WriteLine(Math.Max(52, 273));
+            Console.WriteLine(Math.Min(52, 273));
+            Console.WriteLine(Math.Round(52.273));
+            Console.WriteLine(Math.PI);
 
         }
     }
